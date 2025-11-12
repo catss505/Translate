e0d74424e2971214e43b41c98c7689e48bd6ff4c
Cats505 <noahcanlob@gmail.com>
2025-11-12 20:32:41 +0100

    Create dicionario_de_memes.py

---
+++ dicionario_de_memes.py
@@ -0,0 +1,17 @@
+import time
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Es una risa que se puede referir a algo gracioso",
+            "XD": "Es un emoticono que expresa una risa",
+            "LMAO": "Significa que se parte el culo en pocas palabras"
+            "UWU": "Es un emoticono de una cara"
+            }
+
+while True:
+    word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+    if word in meme_dict.keys():
+        print( meme_dict[word])
+    else:
+        print("Tu palabra no se encuentra en el dicionario")
+    time.sleep(2)
